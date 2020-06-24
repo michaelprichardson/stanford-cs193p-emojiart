e35466f37c1743b8b087ac491107a64cb68db458
Michael Richardson <michael@richardsons.co.za>
2020-06-24 22:19:37 +0100

    [+] Multiple palettes

---
+++ EmojiArt/PaletteChooser.swift
@@ -0,0 +1,33 @@
+//
+//  PaletteChooser.swift
+//  EmojiArt
+//
+//  Created by Michael Richardson on 24/06/2020.
+//  Copyright © 2020 Michael Richardson. All rights reserved.
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    
+    @ObservedObject var document: EmojiArtDocument
+    @Binding var chosenPalette: String
+    
+    var body: some View {
+        HStack {
+            Stepper(onIncrement: {
+                self.chosenPalette = self.document.palette(after: self.chosenPalette)
+            }, onDecrement: {
+                self.chosenPalette = self.document.palette(before: self.chosenPalette)
+            }, label: { EmptyView() })
+            Text(self.document.paletteNames[self.chosenPalette] ?? "")
+        }
+        .fixedSize(horizontal: true, vertical: false)
+    }
+}
+
+struct PaletteChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteChooser(document: EmojiArtDocument(), chosenPalette: Binding.constant(""))
+    }
+}
