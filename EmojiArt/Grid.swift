fdb16f5a3b1c5de2ca15d68aeea1d97bf6fc2344
Michael Richardson <michael@richardsons.co.za>
2020-06-30 17:20:08 +0100

    [+] Added file manager instead of the user defaults.

--- EmojiArt/Grid.swift
+++ EmojiArt/Grid.swift
@@ -2,19 +2,26 @@
 //  Grid.swift
 //  Memorize
 //
-//  Created by Michael Richardson on 31/05/2020.
-//  Copyright © 2020 Michael Richardson. All rights reserved.
+//  Created by CS193p Instructor on 4/8/20.
+//  Copyright © 2020 Stanford University. All rights reserved.
 //
 
 import SwiftUI
 
-struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
-    
+extension Grid where Item: Identifiable, ID == Item.ID {
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.init(items, id: \Item.id, viewForItem: viewForItem)
+    }
+}
+
+struct Grid<Item, ID, ItemView>: View where ID: Hashable, ItemView: View {
     private var items: [Item]
+    private var id: KeyPath<Item,ID>
     private var viewForItem: (Item) -> ItemView
     
-    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+    init(_ items: [Item], id: KeyPath<Item,ID>, viewForItem: @escaping (Item) -> ItemView) {
         self.items = items
+        self.id = id
         self.viewForItem = viewForItem
     }
     
@@ -25,16 +32,20 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
     
     private func body(for layout: GridLayout) -> some View {
-        ForEach(items){ item in
+        return ForEach(items, id: id) { item in
             self.body(for: item, in: layout)
         }
     }
     
     private func body(for item: Item, in layout: GridLayout) -> some View {
-        let index = items.firstIndex(matching: item)!
-        return viewForItem(item)
-            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
-            .position(layout.location(ofItemAt: index))
+        let index = items.firstIndex(where: { item[keyPath: id] == $0[keyPath: id] } )
+        return Group {
+            if index != nil {
+                viewForItem(item)
+                    .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+                    .position(layout.location(ofItemAt: index!))
+            }
+        }
     }
-    
 }
+
